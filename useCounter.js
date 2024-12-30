1854a095f5302bc7be0610c229c07d780c24a79b
Martin Craviotto <martincraviotto@gmail.com>
2024-12-29 21:14:19 -0300

    Primer commit

---
+++ useCounter.js
@@ -0,0 +1,25 @@
+import { useState } from "react"
+
+export const userCounter = (initialValue=10)=>{
+
+    const [counter, setCounter] = useState(initialValue);
+
+    const increment = (value=1) => {
+        setCounter(counter + value);
+    }
+    const decrement = (value=1) => {
+        //if(counter === 1) return;
+        setCounter(counter - value);
+    }
+    const reset = () => {
+        setCounter(initialValue);
+    }
+
+    return{
+        counter,
+        increment,
+        decrement,
+        reset,
+    }
+
+}
